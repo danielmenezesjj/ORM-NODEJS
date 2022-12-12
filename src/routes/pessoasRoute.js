64a7ee00a713fe4f3dcb3a09dd0573cca35a512e
Daniel <88933522+danzinj@users.noreply.github.com>
2022-12-11 21:49:18 -0300

    commit pra estudar crud basico

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -4,6 +4,10 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router();
 
 router.get('/pessoas', PessoaController.listaTodasAsPessoas)
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+router.post('/pessoas', PessoaController.criaPessoa)
+router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.delete('/pessoas/:id', PessoaController.deletaPessoa)
 
 
 module.exports = router
